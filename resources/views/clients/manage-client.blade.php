63167753d6a679134ffeb68ccbac81754d6340fb
omayed <password janina>
2021-09-06 23:20:19 +0600

    Merge branch 'master' of C:\xampp\htdocs\invoice_system with conflicts.

---
+++ resources/views/clients/manage-client.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app', ['page' => __('Manage Invoice'), 'pageSlug' => 'client-manager'])
+@section('content')
+
+@endsection
---
+++ resources/views/clients/new-client.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app', ['page' => __('Manage Invoice'), 'pageSlug' => 'client-create'])
+@section('content')
+<h1>CWSD</h1>
+@endsection
